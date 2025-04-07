29dde31c1379e8d94a71f02f8b28655362af6b9b
hact1v15tx <dhisdickson@gmail.com>
2025-04-07 11:17:39 +0300

    Add inventory_management/inventory/admin.py 1744013859034

--- inventory_management/inventory/admin.py
+++ inventory_management/inventory/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Equipment
 
-# Register your models here.
+@admin.register(Equipment)
+class EquipmentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'type', 'serial_number', 'status', 'assigned_to', 'purchase_date')
+    list_filter = ('type', 'status', 'purchase_date')
+    search_fields = ('name', 'serial_number', 'assigned_to__username')
+    date_hierarchy = 'purchase_date'
+    fieldsets = (
+        ('Basic Information', {
+            'fields': ('name', 'type', 'serial_number')
+        }),
+        ('Status Information', {
+            'fields': ('status', 'assigned_to', 'purchase_date')
+        }),
+        ('Additional Details', {
+            'fields': ('notes',),
+            'classes': ('collapse',)
+        })
+    )
